f186c0f82bd606bd356d1cbb06422a5647819d11
apoutanen <arnim137@gmail.com>
2017-03-04 22:56:27 +0000

    added snake kill avoidance

--- app/main.py
+++ app/main.py
@@ -75,8 +75,8 @@ def move():
     for key in directions:
        #if move will kill snek
         if directions[key][1] == neck or (0 > directions[key][1][0]) or (directions[key][1][0] > width-1) or (0 > directions[key][1][1]) or (directions[key][1][1] > height-1):
-            del directions[key]
-   
+            #del directions[key]
+            directions[key][0] = -2
     
     
    # for snek in allSnakes:
